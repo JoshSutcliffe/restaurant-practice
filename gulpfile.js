d8880d40459e145ed350f9d5ac838cce21fd0ceb
JoshSutcliffe <joshclement12@gmail.com>
2016-12-26 18:32:50 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// var express = require('express');
+// var path = require('path');
+// var app = express();
+// var bodyParser = require("body-parser");
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var browserify = require('browserify');
+
+gulp.task('connect', function () {
+	connect.server({
+		root: 'public',
+		port: 4000
+	})
+});
+
+// gulp.task('browserify', function() {
+// 	// Grabs the app.js file
+//     return browserify('./node_modules/which-country/index.js')
+//     	// bundles it and creates a file called bundle.js
+//         .bundle()
+//         .pipe(source('bundle.js'))
+//         // saves it the public/js/ directory
+//         .pipe(gulp.dest('./public/javascript/'));
+// });
