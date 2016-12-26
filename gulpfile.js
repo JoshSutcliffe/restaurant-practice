3e19ee8b5ceb3e48bba603728f326b60aaa5717a
JoshSutcliffe <joshclement12@gmail.com>
2016-12-26 18:52:56 +0000

    set up sass in styles function

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,26 @@
 // var app = express();
 // var bodyParser = require("body-parser");
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 var browserify = require('browserify');
 
 gulp.task('connect', function () {
 	connect.server({
-		root: 'public',
+		root: 'app',
 		port: 4000
 	})
+});
+
+gulp.task('styles', function() {
+    gulp.src('app/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
 });
 
 // gulp.task('browserify', function() {
