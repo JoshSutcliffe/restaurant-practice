12fdd915459a079d8a52dfd2fd0d2fb1d7bcf50e
JoshSutcliffe <joshclement12@gmail.com>
2016-12-27 21:29:10 +0000

    finishe first stage of https://scotch.io/tutorials/single-page-apps-with-angularjs-routing-and-templating

--- app/js/controllers/controller.js
+++ app/js/controllers/controller.js
@@ -1,7 +1,20 @@
+// FOLLOWING CODE FROM https://scotch.io/tutorials/single-page-apps-with-angularjs-routing-and-templating 
+// create the module and name it scotchApp
 (function() {
-	var app = angular.module("restaurant", []);
+	var scotchApp = angular.module('scotchApp', []);
 
-	app.controller('MainController', ['$scope', function($scope) { 
-	  $scope.title = 'Top Sellers in Books'; 
-	}]);
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+
+	    // create a message to display in our view
+	    $scope.message = 'Everyone come and see how good I look!';
+	});
 })();
+
+// (function() {
+// 	var app = angular.module("restaurant", []);
+
+// 	app.controller('MainController', ['$scope', function($scope) { 
+// 	  $scope.title = 'Top Sellers in Books'; 
+// 	}]);
+// })();
