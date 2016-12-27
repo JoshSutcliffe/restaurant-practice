ebfa711d5d1810ba8b36e1dfa22e13034585be81
JoshSutcliffe <joshclement12@gmail.com>
2016-12-27 22:22:00 +0000

    finishe tute, split angular

--- app/js/controllers/controller.js
+++ app/js/controllers/controller.js
@@ -1,14 +1,46 @@
 // FOLLOWING CODE FROM https://scotch.io/tutorials/single-page-apps-with-angularjs-routing-and-templating 
 // create the module and name it scotchApp
 (function() {
-	var scotchApp = angular.module('scotchApp', []);
+  // create the module and name it scotchApp
+      // also include ngRoute for all our routing needs
+  var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
+  // configure our routes
+  scotchApp.config(function($routeProvider) {
+      $routeProvider
 
-	    // create a message to display in our view
-	    $scope.message = 'Everyone come and see how good I look!';
-	});
+          // route for the home page
+          .when('/', {
+              templateUrl : '../views/home.html',
+              controller  : 'mainController'
+          })
+
+          // route for the about page
+          .when('/about', {
+              templateUrl : '../views/about.html',
+              controller  : 'aboutController'
+          })
+
+          // route for the contact page
+          .when('/contact', {
+              templateUrl : '../views/contact.html',
+              controller  : 'contactController'
+          });
+  });
+
+  // create the controller and inject Angular's $scope
+  scotchApp.controller('mainController', function($scope) {
+      // create a message to display in our view
+      $scope.message = 'Everyone come and see how good I look!';
+  });
+
+  scotchApp.controller('aboutController', function($scope) {
+      $scope.message = 'Look! I am an about page.';
+  });
+
+  scotchApp.controller('contactController', function($scope) {
+      $scope.message = 'Contact us! JK. This is just a demo.';
+  });
 })();
 
 // (function() {
