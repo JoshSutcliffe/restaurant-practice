ae2b721a31f0bc246e9bff162acb780fade5d8b5
JoshSutcliffe <joshclement12@gmail.com>
2016-12-30 13:48:48 +0000

    testing babel

--- app/js/controllers/controller.js
+++ app/js/controllers/controller.js
@@ -7,25 +7,25 @@
 
   // configure our routes
   scotchApp.config(function($routeProvider) {
-      $routeProvider
+    $routeProvider
 
-          // route for the home page
-          .when('/', {
-              templateUrl : '../views/home.html',
-              controller  : 'mainController'
-          })
+	  // route for the home page
+	  .when('/', {
+      templateUrl : '../views/home.html',
+      controller  : 'mainController'
+	  })
 
-          // route for the about page
-          .when('/about', {
-              templateUrl : '../views/about.html',
-              controller  : 'aboutController'
-          })
+	  // route for the about page
+	  .when('/about', {
+      templateUrl : '../views/about.html',
+      controller  : 'aboutController'
+	  })
 
-          // route for the contact page
-          .when('/contact', {
-              templateUrl : '../views/contact.html',
-              controller  : 'contactController'
-          });
+	  // route for the contact page
+	  .when('/contact', {
+      templateUrl : '../views/contact.html',
+      controller  : 'contactController'
+	  });
   });
 
   // create the controller and inject Angular's $scope
--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require("vinyl-source-stream");
 
+// GULP-CONNECT
 gulp.task('connect', function () {
 	connect.server({
 		root: 'app',
@@ -14,14 +17,36 @@ gulp.task('connect', function () {
 	})
 });
 
+// GULP-SASS
 gulp.task('styles', function() {
     gulp.src('app/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('app/css/'));
 });
 
+//Copy static files from html folder to build folder
+// gulp.task("copyStaticFiles", function(){
+//     return gulp.src("./src/html/*.*")
+//     .pipe(gulp.dest("./build"));
+// });
+
+//Convert ES6 code in all js files in src/js folder and copy to 
+//build folder as bundle.js
+gulp.task("build", function(){
+    return browserify({
+        entries: ["./app/js/main.js"]
+    })
+    .transform(babelify.configure({
+        presets : ["es2015"]
+    }))
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("./build"))
+    ;
+});
+
 //Watch task
-gulp.task('default', ['connect', 'styles'], function() {
+gulp.task('default', ['connect', 'styles', 'build'], function() {
     gulp.watch('sass/**/*.scss',['styles']);
 });
 
